db51be15b5250f8804adcb2c1057c5a274d1da80
lleyo <lleyobulsara@gmail.com>
2017-05-05 12:05:21 +0200

    Update dsl.groovy

--- dsl.groovy
+++ dsl.groovy
@@ -1,6 +1,32 @@
 import java.util.List;
 import com.xebialabs.xlrelease.domain.variables.Variable;
 
+/* 
+README 
+In XLR must be configured the following parameters
+	Jenkins server with the following properties
+		Type: jenkins.Server
+		Title: The same value as the variable jenkinsServerTitle in this DSL (i.e: Jenkins orquestacion)
+
+	Global vars:
+		In order to access to jenkins, it need to have defined de password of the jenkins user
+			The global var must be: global.jenkinsPass (type password), and the user will be the user defined in jenkins.Server in shared configuration.
+			It's mandatory to config global.jenkinsPass because the jenkins.Server.userpass cannot be obtained in this DSL (it returns null)
+			
+	Variables in the parent template
+		It's necessary to create the following vars in the template witch will invoke the DSL
+			releaseName
+			releaseVersion
+		Both of them are required and must be asked at the begining of the release, because they are used to set the title of the DSL release
+*/
+
+/* Title of the share configuration for Jenkins */
+def jenkinsServerTitle = "Jenkins orquestacion"
+
+/* Path to the java project that manages the dependencies*/
+def pathRestAPIJar = "C:/proyectos/XLRelease/gitXlRelease/restAPI/target/restAPI-0.1.0-SNAPSHOT.jar"
+
+/* method to obtain a shared configuration */
 def server(type, title) {
   def cis = configurationApi.searchByTypeAndTitle(type, title)
   if (cis.isEmpty()) {
@@ -11,11 +37,22 @@ def server(type, title) {
   }
   cis.get(0)
 }
-def xlJenkinsServer = server('jenkins.Server','Jenkins orquestacion')
 
-def pathRestAPIJar = "C:/proyectos/XLRelease/gitXlRelease/restAPI/target/restAPI-0.1.0-SNAPSHOT.jar"
+def xlJenkinsServer = server('jenkins.Server',jenkinsServerTitle)
 
-def depsScript = "" +
+/* var with the jenkins's pass */
+def jenkinsPass
+
+List<Variable> globalVars = configurationApi.getGlobalVariables()
+for (Variable var : globalVars) {
+	print var.getKey();
+	if ("global.jenkinsPass".equals(var.getKey())) {
+		jenkinsPass = var.getValue();
+	}
+}
+
+/* Python script to execute the Java program for managing dependencies */
+def dependenciesScript = "" +
 "import subprocess\n" +
 "from subprocess import CalledProcessError, check_output\n" +
 "try:\n" +
@@ -28,8 +65,7 @@ def depsScript = "" +
 "    sys.exit(1)\n" +
 "print(output)\n"
 
-print "vars"
-print "${release.id}"
+/* Defining release and version name */
 def releaseName
 def releaseVersion
 List<Variable> vars = releaseApi.getVariables(String.valueOf("${release.id}"))
@@ -38,40 +74,34 @@ for (Variable var : vars) {
 		releaseName = var.getValue();
 	} else if ("releaseVersion".equals(var.getKey())) {
 		releaseVersion = var.getValue();
-	} 
+	}
 }
-print "Name: " + releaseName
-print "Version: " + releaseVersion
 
-print "pinto cosas"
-print releaseName
-
+/* DSL */
 createdRelease = xlr {
     release(releaseName + " " + releaseVersion) {
         description "Hello world release description"
 		variables {
 			stringVariable("statusPlanResult")
 			stringVariable("releaseCreatedId")
-			stringVariable("releaseName")
-			stringVariable("releaseVersion")
 		}	
         phases {
             phase {
                 title "Plan"
                 tasks {
                     script {
-						title "Sample Script task"
-						description "Dynamic script task"
-						script depsScript
+						title "Invoke Java dependencies program"
+						description "Invoke Java dependencies program"
+						script dependenciesScript
 					}
-					custom('Phase status') {
-						description 'Get all done issues'
+					custom('Phase status Plan') {
+						description 'Get Jenkins pipe line status. If it is completed continue'
 						script {
 							type 'jenkins.CheckPipelineStatusPhase'
 							jenkinsServer xlJenkinsServer
 							jobId 'BBVA_Global_Orchestration/job/apx-test/job/develop'
-							username 'ignacio.martin.santamaria.contractor'
-							password 'Password123!'							
+							username xlJenkinsServer.username
+							password jenkinsPass
 							phaseName 'Plan'
 							statusResult variable('statusPlanResult')
 						}
@@ -81,14 +111,14 @@ createdRelease = xlr {
 			phase {
                 title "Build"
                 tasks {                   
-					custom('Phase status') {
-						description 'Get all done issues'
+					custom('Phase status Build') {
+						description 'Get Jenkins pipe line status. If it is completed continue'
 						script {
 							type 'jenkins.CheckPipelineStatusPhase'
 							jenkinsServer xlJenkinsServer
 							jobId 'BBVA_Global_Orchestration/job/apx-test/job/develop'
-							username 'ignacio.martin.santamaria.contractor'
-							password 'Password123!'							
+							username xlJenkinsServer.username
+							password jenkinsPass
 							phaseName 'Build'
 							statusResult variable('statusPlanResult')
 						}
@@ -98,14 +128,14 @@ createdRelease = xlr {
 			phase {
                 title "Unit Test"
                 tasks {                   
-					custom('Phase status') {
-						description 'Get all done issues'
+					custom('Phase status Unit Test') {
+						description 'Get Jenkins pipe line status. If it is completed continue'
 						script {
 							type 'jenkins.CheckPipelineStatusPhase'
 							jenkinsServer xlJenkinsServer
 							jobId 'BBVA_Global_Orchestration/job/apx-test/job/develop'
-							username 'ignacio.martin.santamaria.contractor'
-							password 'Password123!'							
+							username xlJenkinsServer.username
+							password jenkinsPass
 							phaseName 'Unit test'
 							statusResult variable('statusPlanResult')
 						}
